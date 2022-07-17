3c06d7daec5d6583464a75feb3272494fc27ee46
Daniel <darkshoxx@yahoo.de>
2022-07-17 09:21:10 +0100

    included setup, fixes tests

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="randomizer",
+    version="0.1",
+    description="Radomizer",
+    url="https://github.com/darkshoxx/Alt-Tab-Randomizer",
+    author="Daniel Gahler",
+    license="None",
+    packages=find_packages(),
+    install_requires=[],
+)
