14329686df5fde80db3897d804d4d748c8a392de
Daniel Gahler <darkshoxx@yahoo.de>
2022-07-31 16:41:03 +0200

    comments and docstrings in utils.py

--- randomizer/utils.py
+++ randomizer/utils.py
@@ -6,6 +6,7 @@ import win32con as con
 import pywintypes
 
 ### Global constants
+# ScummVM handles that aren't the game.
 LIST_OF_WRONG_WINDOWS = [
     "MSCTFIME UI",
     "Default IME",
@@ -13,11 +14,13 @@ LIST_OF_WRONG_WINDOWS = [
     "__wglDummyWindowFodder",
     "NVOGLDC invisible"
     ]
+# required for usage of api.OpenProcess()
 QUERY_INFO = con.PROCESS_QUERY_INFORMATION
 VM_READ = con.PROCESS_VM_READ
 
 def get_all_handles() -> List:
-    """Returns a list of all handles"""
+    """Returns a list of all handles. Obtains curent foreground window and 
+    iterates through windows in front and behind."""
     current_handle = gui.GetForegroundWindow()
     list_of_handles = [current_handle]
     next_handles = get_half_handles(current_handle, "next")
@@ -26,7 +29,17 @@ def get_all_handles() -> List:
     list_of_handles += previous_handles
     return list_of_handles
 
-def get_half_handles(current_handle, direction):
+def get_half_handles(
+    current_handle: List[int],
+    direction: str
+    )-> List[int]:
+    """Helper function to iterate over all handles in front/behind the active 
+    window handle.
+    Args:
+        current_handle (List[int]): singleton list containing active window
+        direction (str): next/previous for search direciton.
+    Returns:
+        half_handles_list (List[int]): collected list of handles."""
     if direction == "next":
         direction_int = 3
     elif direction == "previous":
@@ -35,6 +48,8 @@ def get_half_handles(current_handle, direction):
         raise Exception("Invalid search direction")
     got_a_new_window = True
     half_handles_list = []
+    # filling the list using gui.GetWindow, which uses direction_int to specify
+    # next or previous window.
     while(got_a_new_window):
         current_handle = gui.GetWindow(current_handle, direction_int)
         if (current_handle not in half_handles_list) and current_handle != 0:
@@ -44,6 +59,11 @@ def get_half_handles(current_handle, direction):
     return half_handles_list
 
 def filter_handles_by_exe_name(list_of_handles):
+    """Helper function to remove all handles that do not originate from ScummVM
+    Args:
+        list_of_handles (List[int]): List of all handles
+    Returns:
+        scummvm_handles (List[int]): List of handles belonging to ScummVM."""
     scummvm_handles = []
     for handle in list_of_handles:
         ident_b = get_process_id_from_handle(handle)
@@ -54,16 +74,25 @@ def filter_handles_by_exe_name(list_of_handles):
             #print("an error has occured")
             continue
         if exe_name[-11:] == "scummvm.exe":
+            # removes handles that are from ScummVM but known not to be
+            # the actual game window
             if gui.GetWindowText(handle) not in LIST_OF_WRONG_WINDOWS:
                 scummvm_handles.append(handle)
                 print(gui.GetWindowText(handle))
     return scummvm_handles
 
-def get_process_id_from_handle(handle):
+def get_process_id_from_handle(handle: int):
+    """Gets process id from the window handle. Required to find exe
+    Args:
+        handle (int): handle to obtain id from
+    Returns:
+        ident_b: PID for exe    
+    """
     _, ident_b = processes.GetWindowThreadProcessId(handle)
     return ident_b
 
-def get_exe_from_process_id(ident_b):
+def get_exe_from_process_id(ident_b)-> str:
+    """Gets the process handle from the PID, returns string of exe"""
     process_handle_b = api.OpenProcess(QUERY_INFO | VM_READ, False, ident_b)
     exe_name = processes.GetModuleFileNameEx(process_handle_b, 0)
     return exe_name
